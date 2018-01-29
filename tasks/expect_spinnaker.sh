4fd728b5d413a8ff99eab241d163166262cbca9d
devlet <jatinbajaj.mca@gmail.com>
2018-01-29 23:46:20 +0530

    add files

---
+++ tasks/expect_spinnaker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect -f
+ 
+ 
+spawn /home/devlet/spinnaker_ansible/files/install_spinnaker.sh
+ 
+
+set timeout 10s
+ 
+expect "Please supply a non-root user to run Halyard as:"
+
+send --  "devlet\n" 
+
+set timeout 300s
+
+expect eof 
---
+++ tasks/install_spinnaker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+
+curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
+sudo bash InstallHalyard.sh
