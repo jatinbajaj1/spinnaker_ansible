3611c4428f8dd7c68b23e64fe2a12b8ac150d20d
devlet <jatinbajaj.mca@gmail.com>
2018-02-06 11:43:50 +0530

    Changes

--- install_all.sh
+++ install_all.sh
@@ -4,4 +4,10 @@ sudo apt-get update
 sudo apt-get install ansible
 sudo apt-get install python-pip
 
+sudo apt-get install git 
+
+sudo git clone https://github.com/jatinbajaj1/spinnaker_ansible.git
+
+cd spinnaker_ansible
+
 sudo ansible-playbook -i defaults/inventory  tasks/main.yml  -vvv
