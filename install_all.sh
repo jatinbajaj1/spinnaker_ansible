a29f1411281a70e87cfb0af7dee5bd0b68518a6e
jatinbajaj1 <jatinbajaj.mca@gmail.com>
2018-02-27 10:57:29 +0530

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -4,7 +4,11 @@ sudo apt-get update
 sudo apt-get install ansible
 sudo apt-get install python-pip
 
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl
+sudo chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
 sudo apt-get install git 
+
 
 sudo git clone https://github.com/jatinbajaj1/spinnaker_ansible.git
 
